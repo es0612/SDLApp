c2dd43eb48cdc6f4f773ec1399e15e68ebb5768f
harami <harami@example.com>
2019-08-04 22:44:38 +0900

    add state button settings for screen

--- app/SdlSwiftApp/ProxyManager.swift
+++ app/SdlSwiftApp/ProxyManager.swift
@@ -77,11 +77,16 @@ class ProxyManager: NSObject, SDLManagerDelegate {
             // アプリから車載機の画面アップデートする際に必須
             sdlManager.screenManager.beginUpdates()
 
-            //更新処理
+            // 更新処理 text
             sdlManager.screenManager.textField1 = "text 1"
             sdlManager.screenManager.textField1 = "text 2"
             sdlManager.screenManager.textField1 = "text 3"
             sdlManager.screenManager.textField1 = "text 4"
+
+            // 更新処理　ボタン
+            let stateButton = setStateButton()
+            sdlManager.screenManager.softButtonObjects = [stateButton]
+
 
             // アプリから車載機の画面アップデートする際に必須
             sdlManager.screenManager.endUpdates { (error) in
@@ -92,5 +97,33 @@ class ProxyManager: NSObject, SDLManagerDelegate {
                 }
             }
         }
+    }
+
+    func setStateButton() -> SDLSoftButtonObject {
+        let image1 = UIImage(named: "spring.png")
+        let artwork1 = SDLArtwork(image: image1!, persistent: true, as: .PNG)
+        let image2 = UIImage(named: "summer.png")
+        let artwork2 = SDLArtwork(image: image2!, persistent: true, as: .PNG)
+        let image3 = UIImage(named: "autumn.png")
+        let artwork3 = SDLArtwork(image: image3!, persistent: true, as: .PNG)
+        let image4 = UIImage(named: "winter.png")
+        let artwork4 = SDLArtwork(image: image4!, persistent: true, as: .PNG)
+
+        let sbState11 = SDLSoftButtonState(stateName: "State11", text: "spring", artwork: artwork1)
+        let sbState12 = SDLSoftButtonState(stateName: "State12", text: "summer", artwork: artwork2)
+        let sbState13 = SDLSoftButtonState(stateName: "State13", text: "autumn", artwork: artwork3)
+        let sbState14 = SDLSoftButtonState(stateName: "State14", text: "winter", artwork: artwork4)
+
+        let sbObj1 = SDLSoftButtonObject(name: "Button1", states: [sbState11, sbState12, sbState13, sbState14], initialStateName: "State11")
+        {
+            (buttonPress, buttonEvent) in
+
+            guard buttonPress != nil else { return }
+            print("Button 1 Pressed!")
+            let sbObj = self.sdlManager.screenManager.softButtonObjectNamed("Button1")
+            sbObj?.transitionToNextState()
+        }
+
+        return sbObj1
     }
 }
