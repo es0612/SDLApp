9185cba7abdbe0d8058dfc4360503c6251323486
hyai <hyai0323@gmail.com>
2019-08-26 23:27:35 +0900

    clean code of proxy manager.

--- app/SdlSwiftApp/ProxyManager.swift
+++ app/SdlSwiftApp/ProxyManager.swift
@@ -2,14 +2,14 @@ import Foundation
 
 import SmartDeviceLink
 
-class ProxyManager: NSObject, SDLManagerDelegate {
+class ProxyManager: NSObject {
 
     fileprivate var sdlManager: SDLManager!
 
     // singleton
     static let sharedManager = ProxyManager()
 
-    let appName = "SdlSwiftApp"
+    let appName = "doNothingApp"
     let ipAddress = "m.sdl.tools"
     var port: UInt16!
 
@@ -25,10 +25,47 @@ class ProxyManager: NSObject, SDLManagerDelegate {
         }
     }
 
+    // MARK: - local Methods
+    func updateScreen() {
+        sdlManager.screenManager.beginUpdates()
+
+        setScreenTemplete()
+        sdlManager.screenManager.textField1 = "喋れ！！！"
+
+        guard let appImage = UIImage(named: "sdlicon.png") else {return}
+        sdlManager.screenManager.primaryGraphic
+            = SDLArtwork(
+                image: appImage,
+                name: "mfsdlapp.png",
+                persistent: true,
+                as: .PNG
+        )
+
+        sdlManager.screenManager.endUpdates { (error) in
+            if error == nil {
+                print("UI updated.")
+            } else {
+                print("UI update failed. Error: \(String(describing: error))")
+            }
+        }
+    }
+
+    func setScreenTemplete() {
+        let display = SDLSetDisplayLayout(predefinedLayout: .textWithGraphic)
+
+        sdlManager.send(request: display) { (request, response, error) in
+            if response?.resultCode == .success {
+                print("The template has been set successfully")
+            }
+        }
+    }
+}
+
+extension ProxyManager: SDLManagerDelegate {
     func setUp() {
         let lifecycleConfiguration = SDLLifecycleConfiguration(
             appName: appName,
-            fullAppId: "1234",
+            fullAppId: "0001",
             ipAddress: ipAddress,
             port: port
         )
@@ -67,46 +104,8 @@ class ProxyManager: NSObject, SDLManagerDelegate {
     func hmiLevel(_ oldLevel: SDLHMILevel, didChangeToLevel newLevel: SDLHMILevel) {
         print("Went from HMI level:\(oldLevel) to HMI level:\(newLevel)")
 
-        // 画面の更新はlevelがfullの時に行う
         if newLevel == .full {
-            // アプリから車載機の画面アップデートする際に必須
-            sdlManager.screenManager.beginUpdates()
-
-            // 画面テンプレート　変更
-            setScreenTemplete()
-
-            // 更新処理 text
-            sdlManager.screenManager.textField1 = "喋れ！！！"
-
-            // 画像更新
-            guard let appImage = UIImage(named: "sdlicon.png") else {return}
-            sdlManager.screenManager.primaryGraphic
-                = SDLArtwork(
-                    image: appImage,
-                    name: "mfsdlapp.png",
-                    persistent: true,
-                    as: .PNG
-            )
-
-            // アプリから車載機の画面アップデートする際に必須
-            sdlManager.screenManager.endUpdates { (error) in
-                if error == nil {
-                    print("UI updated.")
-                } else {
-                    print("UI update failed. Error: \(String(describing: error))")
-                }
-            }
-        }
-    }
-
-    // MARK: - local Methods
-    func setScreenTemplete() {
-        let display = SDLSetDisplayLayout(predefinedLayout: .textWithGraphic)
-
-        sdlManager.send(request: display) { (request, response, error) in
-            if response?.resultCode == .success {
-                print("The template has been set successfully")
-            }
+            updateScreen()
         }
     }
 }
