0d14067e874d4bb7b782cf83febc60fd99f6fcd3
harami <harami@example.com>
2019-08-27 15:07:29 +0900

    get speed data

--- app/SdlSwiftApp/ProxyManager.swift
+++ app/SdlSwiftApp/ProxyManager.swift
@@ -2,16 +2,18 @@ import Foundation
 
 import SmartDeviceLink
 
+
 class ProxyManager: NSObject {
 
     fileprivate var sdlManager: SDLManager!
-
-    // singleton
     static let sharedManager = ProxyManager()
 
     let appName = "doNothingApp"
     let ipAddress = "m.sdl.tools"
     var port: UInt16!
+
+    var firstHmiState: SDLHMILevel = .none
+    var speedValue = "0.0"
 
     func connect() {
         setUp()
@@ -19,48 +21,18 @@ class ProxyManager: NSObject {
         sdlManager.start { (success, error) in
             if success {
                 print("Connected.")
+                self.sdlManager.subscribe(to: .SDLDidReceiveVehicleData,
+                                          observer: self,
+                                          selector: #selector(self.vehicleDataNotification(_:))
+                )
             } else {
                 print("Connection failed.")
             }
         }
     }
-
-    // MARK: - local Methods
-    func updateScreen() {
-        sdlManager.screenManager.beginUpdates()
-
-        setScreenTemplete()
-        sdlManager.screenManager.textField1 = "喋れ！！！"
-
-        guard let appImage = UIImage(named: "sdlicon.png") else {return}
-        sdlManager.screenManager.primaryGraphic
-            = SDLArtwork(
-                image: appImage,
-                name: "mfsdlapp.png",
-                persistent: true,
-                as: .PNG
-        )
-
-        sdlManager.screenManager.endUpdates { (error) in
-            if error == nil {
-                print("UI updated.")
-            } else {
-                print("UI update failed. Error: \(String(describing: error))")
-            }
-        }
-    }
-
-    func setScreenTemplete() {
-        let display = SDLSetDisplayLayout(predefinedLayout: .textWithGraphic)
-
-        sdlManager.send(request: display) { (request, response, error) in
-            if response?.resultCode == .success {
-                print("The template has been set successfully")
-            }
-        }
-    }
 }
 
+// MARK: - delegate Methods
 extension ProxyManager: SDLManagerDelegate {
     func setUp() {
         let lifecycleConfiguration = SDLLifecycleConfiguration(
@@ -96,16 +68,95 @@ extension ProxyManager: SDLManagerDelegate {
         )
     }
 
-    // MARK: - delegate Methods
     func managerDidDisconnect() {
         print("Manager disconnected.")
     }
 
     func hmiLevel(_ oldLevel: SDLHMILevel, didChangeToLevel newLevel: SDLHMILevel) {
-        print("Went from HMI level:\(oldLevel) to HMI level:\(newLevel)")
+
+        if newLevel != .none && firstHmiState == .none {
+            firstHmiState = newLevel
+
+            setSubscribeSpeed()
+        }
 
         if newLevel == .full {
             updateScreen()
         }
+    }
+}
+
+// MARK: - local Methods
+private extension ProxyManager {
+    func updateScreen() {
+        sdlManager.screenManager.beginUpdates()
+
+        setScreenTemplete()
+
+        sdlManager.screenManager.textField1 = "speed"
+        sdlManager.screenManager.textField2 = speedValue + " kph"
+
+        guard let appImage = UIImage(named: "sdlicon.png") else {return}
+        sdlManager.screenManager.primaryGraphic
+            = SDLArtwork(
+                image: appImage,
+                name: "mfsdlapp.png",
+                persistent: true,
+                as: .PNG
+        )
+
+        sdlManager.screenManager.endUpdates { (error) in
+            if error == nil {
+                print("UI updated.")
+            } else {
+                print("UI update failed. Error: \(String(describing: error))")
+            }
+        }
+    }
+
+    func setScreenTemplete() {
+        let display = SDLSetDisplayLayout(predefinedLayout: .textWithGraphic)
+
+        sdlManager.send(request: display) { (request, response, error) in
+            if response?.resultCode == .success {
+                print("The template has been set successfully")
+            }
+        }
+    }
+}
+
+// vehicle data
+extension ProxyManager {
+
+    func setSubscribeSpeed() {
+        let subscribeSpeed = SDLSubscribeVehicleData()
+        subscribeSpeed.speed = true as NSNumber
+
+        sdlManager.send(request: subscribeSpeed) { (request, response, error) in
+            guard let response = response as? SDLSubscribeVehicleDataResponse else { return }
+            guard response.resultCode == .success
+                else {
+                    switch response.resultCode
+                    {
+                    case .disallowed:
+                        print("disallowd")
+                    default:
+                        print("default")
+                    }
+
+                    return
+            }
+        }
+    }
+
+    @objc func vehicleDataNotification(_ notification: SDLRPCNotificationNotification) {
+        guard let onVehicleData = notification.notification as? SDLOnVehicleData,
+            let speed = onVehicleData.speed
+            else {
+                return
+        }
+
+        speedValue = speed.stringValue
+        updateScreen()
     }
 }
